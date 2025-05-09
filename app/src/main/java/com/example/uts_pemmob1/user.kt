d5d9783ea672ed5c6bd898fcf1a8a509bea12527
Ilmi-Restu-Firdaus <151982742+Ilmi-Restu-Firdaus@users.noreply.github.com>
2025-05-10 06:29:39 +0700

    Update user.kt

--- app/src/main/java/com/example/uts_pemmob1/user.kt
+++ app/src/main/java/com/example/uts_pemmob1/user.kt
@@ -1,7 +1,10 @@
 package com.example.uts_pemmob1
 
+// Data class untuk menyimpan informasi pengguna
 data class User(val username: String, val password: String)
 
+// Objek UserData untuk menyimpan daftar pengguna (userList)
 object UserData {
+    // Menggunakan ArrayList untuk menyimpan data pengguna
     val userList = ArrayList<User>()
 }
